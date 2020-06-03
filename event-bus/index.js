39dc06b52b02239cfa9ea128f3bc1934ed7c2f35
Lior-Zada <lior305@gmail.com>
2020-06-03 22:57:00 +0300

    Events are now stored, so that if a service goes offline, it can sync back

--- event-bus/index.js
+++ event-bus/index.js
@@ -7,19 +7,23 @@ const app = express();
 
 app.use(bodyParser.json());
 
+const events = [];
+
 app.post('/events', (req, res) => {
     const event = req.body;
-
-    try {
+    events.push(event); // Store events for recovery or syncing new services.
+    // try {
         axios.post('http://localhost:4000/events', event); //update Posts service
         axios.post('http://localhost:4001/events', event); //update Comments service
-        axios.post('http://localhost:4002/events', event); //update Query service    
+        axios.post('http://localhost:4002/events', event).then(c=>'').catch(a=>console.log('fail')); //update Query service    
         axios.post('http://localhost:4003/events', event); //update Moderation service    
 
         res.send({status: 'Event registered', success: true});
-    } catch (error) {
-        res.send({status: 'Failed updating one of the services', success: false});
-    }
+    // } catch (error) {
+        // res.send({status: 'Failed updating one of the services', success: false});
+    // }
 });
+
+app.get('/events', (req, res) => res.send(events));
 
 app.listen(port, () => console.log(`Event bus listening on ${port}`));
--- query/index.js
+++ query/index.js
@@ -2,20 +2,28 @@ const express = require('express');
 const port = 4002;
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const axios = require('axios');
 const serviceName = 'Query service';
 const app = express();
+
 app.use(bodyParser.json());
 app.use(cors());
 
 const posts = {};
 
 app.post('/events', (req, res) => {
-    console.log(`${serviceName} received event ${req.body.type}`);
-    const { type, postId, commentId, title, content, status } = req.body;
+    handleEvent(req.body);
+    res.send({});
+});
+
+const handleEvent = (data) => {
+    const { type, postId, commentId, title, content, status } = data;
+    
+    console.log(`${serviceName} received event ${type}`);
 
     switch (type) {
         case 'NEW_POST':
-            posts[postId] = { id: postId, title, comments: [] };
+            posts[postId] = { id: postId, title, comments: []};
             break;
 
         case 'NEW_COMMENT':
@@ -38,12 +46,13 @@ app.post('/events', (req, res) => {
         default:
             break;
     }
-    // console.log(posts);
-    res.send({});
-});
-
+}
 app.get('/posts', (req, res) => {
     res.send(posts);
 });
 
-app.listen(port, () => console.log(`Query service listening on ${port}`));
+app.listen(port, async () => {
+    console.log(`Query service listening on ${port}`);
+    const res = await axios.get('http://localhost:4006/events');
+    res.data.forEach(event => handleEvent(event));
+});
